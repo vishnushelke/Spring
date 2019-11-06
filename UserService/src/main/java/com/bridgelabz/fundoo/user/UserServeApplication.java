9ca451ce80a735e8694f35d4413d91b01de1f0aa
vishnu shelke <shelkeva@gmail.com>
2019-11-06 09:19:56 +0530

    collaborator done

--- UserService/src/main/java/com/bridgelabz/fundoo/user/UserServeApplication.java
+++ UserService/src/main/java/com/bridgelabz/fundoo/user/UserServeApplication.java
@@ -12,10 +12,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.bridgelabz.fundoo.user.note.model.Note;
 @EnableCaching
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UserServeApplication {
 
+	/**
+	 * purpose: this method creates a bean of jedisConnectionFactory. We need to have a connection 
+	 * 			factory to connect to the redis server
+	 * @return jedisConnectionFactory 
+	 */
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+
+	@Bean
+	RedisTemplate<String, Note> redisTemplate() {
+		RedisTemplate<String, Note> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		return redisTemplate;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UserServeApplication.class, args);
 	}
